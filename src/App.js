87edc4448aeb2d2c962c1a9c223b3bf748da19f4
Monick <monick.keo@gmail.com>
2019-12-17 10:05:03 -0800

    added routing for components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Nav from './Nav.js'
 import MovieSearch from './components/MovieSearch.js';
 import MovieLibrary from './components/MovieLibrary.js';
 import CustomerList from './components/CustomerList.js';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Nav/>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav/>
+          <Route path='/moviesearch' component={MovieSearch}/>
+          <Route path='/movielibrary' component={MovieLibrary}/>
+          <Route path='/customerlist' component={CustomerList}/>
+          {/* <MovieSearch/> */}
+          {/* <MovieLibrary/>
+          <CustomerList/> */}
+        </div>
+      </Router>
     );
   }
 }
