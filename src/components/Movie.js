e1d3596840c56e1ff0b422fcf16f8b299bb36f6b
Monick <monick.keo@gmail.com>
2019-12-17 21:52:00 -0800

    able to get function for displaying searched movie but not complete

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 class Movie extends Component {
-  // constructor(props){
-  //   super(props)
-  // }
-
   render() {
     return (
       <div>
